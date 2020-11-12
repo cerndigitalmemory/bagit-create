5e942efc0abff3219f5dc176978cc1010527e20d
Antonio Vivace <avivace4@gmail.com>
2020-11-12 17:51:16 +0100

    Fix AIC creation

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 from fs import open_fs
 import fs
-my_fs = open_fs('.')
 import os
 import time
-
 import random
 import string
-
 import cds
-
 import click
 
+my_fs = open_fs('.')
+
+
 def get_random_string(length):
-    letters = string.ascii_lowercase
-    result_str = ''.join(random.choice(letters) for i in range(length))
-    return result_str
+	"""
+	Get a random string of the desired length
+	"""
+	letters = string.ascii_lowercase
+	result_str = ''.join(random.choice(letters) for i in range(length))
+	return result_str
 
 # Stub
+
+
 def checkunique(id):
+	"""
+	Check if the given ID is unique in our system
+	"""
 	print("ID is unique")
 	return True
+
 
 def generateReferences(filepathslist):
 	"""
@@ -36,92 +44,102 @@ def generateReferences(filepathslist):
 		references += "\n"
 	return references
 
-def getHash(filename):
+
+def getHash(filename, alg="md5"):
 	"""
 	Compute hash of a given file
 	"""
-	computedhash = my_fs.hash(filename, "md5") + get_random_string(5)
+	computedhash = my_fs.hash(filename, alg) + get_random_string(5)
 	# TODO: remove the random part with real payloads
 	return computedhash
 
 
-@click.command()
-@click.option('--foldername', default="1", help='ID of the resource')
-@click.option('--method', help='Processing method to use')
+#@click.command()
+#@click.option('--foldername', default="1", help='ID of the resource')
+#@click.option('--method', help='Processing method to use')
 def process(foldername, method, timestamp=0, requestedFormat="MP4"):
-	print(foldername, method)
 	# Check if the target name is actually unique
 	checkunique(foldername)
-	
+
 	# Get current path
 	path = os.getcwd()
 
 	# Prepare the high level folder which will contain the AIC and the AIUs
-	baseexportpath = "bagitexport_"+foldername
-	os.mkdir(path+"/"+baseexportpath)
-	
+	baseexportpath = "bagitexport_" + foldername
+	os.mkdir(path + "/" + baseexportpath)
+
 	# If we're on CDS1 pipeline, create also the source folder,
-	#  we will need to fetch the raw files, too
+	#  since we will need to fetch the raw files
 	if method == "cds":
-		os.mkdir(path+'/'+foldername)
-		
-	# Create the AIC folder
+		os.mkdir(path + '/' + foldername)
+
+	# Create the AIC folder (ResourceID_timestamp)
 	if timestamp == 0:
 		print("No timestamp provided. Using 'now'")
 		timestamp = int(time.time())
-
-	aicfoldername = baseexportpath+"/"+foldername+"_"+str(timestamp)
+	aicfoldername = baseexportpath + "/" + foldername + "_" + str(timestamp)
 	print("AIC folder name is", aicfoldername)
-	os.mkdir(path+"/"+aicfoldername)
-	
+	os.mkdir(path + "/" + aicfoldername)
+
 	# CDS Pipeline
 	if method == "cds":
 		print("Fetching the CDS Resource", foldername)
 
 		# Get and save metadata
 		metadata = cds.getMetadata(foldername)
-		open(path+'/'+foldername +'/' + "metadata.xml", 'wb').write(metadata)
+		open(path + '/' + foldername + '/' +
+			 "metadata.xml", 'wb').write(metadata)
 		print("Getting source files locations")
+
 		# From the metadata, extract info about the upstream file sources
-		files = cds.getRawFilesLocs(foldername+"/metadata.xml")
+		files = cds.getRawFilesLocs(foldername + "/metadata.xml")
 		print("Got", len(files), "sources")
-		print("Looking for", requestedFormat ,"file..")
+		print("Looking for an", requestedFormat, "file..")
 		for sourcefile in files:
 			if sourcefile["filetype"] == requestedFormat:
 				print("Downloading", sourcefile["url"])
-				# slow connection
-				# cds.downloadRemoteFile(sourcefile["url"], ".")
-				open(path+'/'+foldername +'/' + foldername +".mp4", 'wb').write(b"DUMMYDATA")
+				#Slow connection workaround
+				filedata = cds.downloadRemoteFile(sourcefile["url"], path + '/' + foldername + '/' +  foldername + ".mp4")
+				#open(path + '/' + foldername + '/' +
+				#	 foldername + ".mp4", 'wb').write(filedata)
 
 	# Prepare AIC
 	filelist = []
+
 	for el in my_fs.scandir(foldername):
 		if el.is_dir:
-			for file in my_fs.listdir(foldername+'/'+el.name):
+			for file in my_fs.listdir(foldername + '/' + el.name):
 				filepath = foldername + "/" + el.name + "/" + file
 				filelist.append(filepath)
+		else:
+			filepath = foldername + "/" + el.name
+			filelist.append(filepath)
+
+	print("Filelist:",filelist)
+
 	references = generateReferences(filelist)
-	my_fs.writetext(aicfoldername+"/"+'references.txt', references)
-				
+	
+	my_fs.writetext(aicfoldername + "/" + 'references.txt', references)
+
 	# Look for high-level metadata and copy it into the AIC
 	metadatafilenames = ["metadata.json", "metadata.xml"]
 
 	for filename in metadatafilenames:
 		if filename in my_fs.listdir(foldername):
-			my_fs.copy(foldername + "/"+filename, aicfoldername+filename)
+			my_fs.copy(foldername + "/" + filename, aicfoldername + '/' + filename)
 
 	# AIUs
 	for file in filelist:
 		filehash = getHash(file)
-		aiufoldername =baseexportpath+"/"+ foldername+"_"+filehash
+		aiufoldername = baseexportpath + "/" + foldername + "_" + filehash
 		os.mkdir(aiufoldername)
-		my_fs.copy(file, aiufoldername+"/"+fs.path.basename(file))
+		my_fs.copy(file, aiufoldername + "/" + fs.path.basename(file))
 
 
-# standardarchive -> 
+# standardarchive ->
 # process("photoid-2704179", "transfermode", timestamp=12031239)
 
-# process("2272168", "cds")
+process("2272168", "cds")
 
-if __name__ == '__main__':
-    process()
+#if __name__ == '__main__':
+#	process()
