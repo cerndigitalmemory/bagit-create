17dc779db1c28cbc3476a2a01d6e028ef6bcacfd
Antonio Vivace <avivace4@gmail.com>
2021-08-04 01:20:55 +0200

    base: print final result and bic-meta location

--- bagit_create/main.py
+++ bagit_create/main.py
@@ -94,12 +94,15 @@ def process(
         pipeline.verify_bag(base_path)
 
         pipeline.delete_folder(temp_files_path)
+        return f"SUCCESS. Final bic-meta wrote in {aic_path}/bic-meta.json"
     except FileExistsError as e:
         # Folder exists, gracefully stop.
         logging.error(f"Job failed with error: {e}")
+        return "FAIL"
     except Exception as e:
         # For any other error, print details and clean up
         #  any folder created
         logging.error(f"Job failed with error: {e}")
         pipeline.delete_folder(temp_files_path)
         pipeline.delete_folder(base_path)
+        return "FAIL"
