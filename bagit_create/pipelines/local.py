8a986fab564bea20f7382ce105536e77454c6e2a
kchelaki <konstantinos.chelakis@cern.ch>
2021-10-21 16:17:02 +0200

    local mode follows system links

--- bagit_create/pipelines/local.py
+++ bagit_create/pipelines/local.py
@@ -1,4 +1,4 @@
-from posixpath import relpath
+from posixpath import islink, relpath
 from . import base
 import logging
 import fs
@@ -12,6 +12,7 @@ from pwd import getpwuid
 from os import walk
 import checksumdir
 import hashlib
+import shutil
 
 log = logging.getLogger("basic-logger")
 
@@ -37,46 +38,44 @@ class LocalV1Pipeline(base.BasePipeline):
         if os.path.isfile(src):
             file = ntpath.basename(src)
             dirpath = ntpath.dirname(src)
-            relpath = os.path.basename(os.path.normpath(dirpath))
-            obj = self.get_local_metadata(
-                file, src, dirpath, relpath, base_name, authorSourcePath, isFile=True
-            )
+            obj = self.get_local_metadata(file, src, dirpath, isFile=True)
             files.append(obj)
         else:
             # Walk through the whole directory and prepare an object for each found file
-            for (dirpath, dirnames, filenames) in walk(src):
-                relpath = os.path.basename(os.path.normpath(dirpath))
+            for (dirpath, dirnames, filenames) in walk(src, followlinks=True):
                 for file in filenames:
                     obj = self.get_local_metadata(
                         file,
                         src,
                         dirpath,
-                        relpath,
-                        base_name,
-                        authorSourcePath,
                         isFile=False,
                     )
                     files.append(obj)
-
-        # I don't have metadata json here. If we add, fields will be added here
         return files
 
     def copy_files(self, files, source_dir, dest_dir):
         my_fs = open_fs("/")
         if os.path.isfile(source_dir):
-            fs.copy.copy_file(
-                src_fs=my_fs,
-                dst_fs=my_fs,
-                src_path=f"{source_dir}",
-                dst_path=f"{dest_dir}/{ntpath.basename(source_dir)}",
+            shutil.copy(
+                f"{source_dir}",
+                f"{dest_dir}/{ntpath.basename(source_dir)}",
             )
         else:
-            fs.copy.copy_dir(
-                src_fs=my_fs,
-                dst_fs=my_fs,
-                src_path=f"{source_dir}",
-                dst_path=f"{dest_dir}",
-            )
+            for (dirpath, dirnames, filenames) in walk(source_dir, followlinks=True):
+
+                if source_dir == dirpath:
+                    target = dest_dir
+                else:
+                    dest_relpath = dirpath[len(source_dir) + 1 :]
+                    target = f"{dest_dir}/{dest_relpath}"
+
+                try:
+                    os.mkdir(target)
+                except:
+                    pass
+
+                for file in filenames:
+                    shutil.copy(f"{os.path.abspath(dirpath)}/{file}", target)
 
         for file in files:
             file["downloaded"] = True
@@ -114,25 +113,25 @@ class LocalV1Pipeline(base.BasePipeline):
             self.write_file(content, f"{base_path}/manifest-{alg}.txt")
         return files
 
-    def get_local_metadata(
-        self, file, src, dirpath, relpath, base_name, authorSourcePath, isFile
-    ):
+    def get_local_metadata(self, file, src, dirpath, isFile):
         # Prepare the File object
         obj = {"origin": {}}
 
         obj["origin"]["filename"] = file
         ## If you are in the root directory just use filename
+
         if dirpath == src or isFile:
             obj["origin"]["path"] = ""
             sourcePath = f"{file}"
         # Otherwise prepare the relative path
         else:
-            relpath = dirpath[len(src) :]
+            relpath = dirpath[len(src) + 1 :]
             obj["origin"]["path"] = relpath
             sourcePath = f"{relpath}/{file}"
-
         obj["origin"]["sourcePath"] = f"{dirpath}/{file}"
+
         obj["bagpath"] = f"data/content/{sourcePath}"
+
         try:
             obj["size"] = os.path.getsize(f"{dirpath}/{file}")
         except OSError:
