1fd1e1d4608f6ca692f49656e824b62e594e4762
Antonio Vivace <avivace4@gmail.com>
2022-07-26 16:53:03 +0200

    Append the head short commit hash to the version when running from a git cloned copy

--- bagit_create/version.py
+++ bagit_create/version.py
@@ -1,1 +1,39 @@
-__version__ = "0.1.9"
+import os
+import subprocess
+
+# Change this when releasing a new version
+VERSION = "0.1.9"
+
+
+def get_commit_hash():
+    """
+    This function tries to run git from the parent directory of this file,
+    (which should be the main repository folder of bagit-create) to get the current commit hash.
+    This will return a short git commit hash if BIC is being executed from a local git copy,
+    None otherwise (e.g. when installing a released version from PyPi)
+    """
+    try:
+        bagit_base_path = os.path.dirname(__file__)
+        git_output = subprocess.check_output(
+            ["git", "rev-parse", "--short", "HEAD"], cwd=bagit_base_path
+        )
+        short_hash = git_output.decode().rstrip()
+    except Exception:
+        short_hash = None
+    return short_hash
+
+
+def get_version():
+    """
+    Returns a string like `bic, version 0.1.9 git/47278a2` when running from a git cloned copy of BIC
+    or simply `bic, version 0.1.9` when running from a released version of the tool
+    """
+    git_commit = get_commit_hash()
+
+    if git_commit is not None:
+        return f"{VERSION}-git{git_commit}"
+    else:
+        return VERSION
+
+
+__version__ = get_version()
