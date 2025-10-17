d8bd5536151f64d71572184634f0e7d91230b268
Diana Rand <diana.rand@cern.ch>
2025-10-17 17:08:33 +0200

    update pymarc version

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
         "mccabe==0.6.1",
         "pycodestyle==2.7.0",
         "pyflakes==2.3.0",
-        "pymarc==4.0.0",
+        "pymarc==4.1.3",
         "pytz==2021.3",
         "requests>=2.26",
         "six==1.15.0",
