3ffc2aa5334526e57dd94f70daa6ba9aef20c6b3
Panna Liptak <panna.georgina.liptak@cern.ch>
2026-04-28 11:51:37 +0200

    Test sanitization

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
         "warcio==1.7.5",
         "crawlee==1.1.0",
         "beautifulsoup4==4.14.2",
-        "oais_utils>=0.4.0,<1.0.0",
+        "oais_utils @ git+https://gitlab.cern.ch/digitalmemory/utils.git@sanitization2",
     ],
     entry_points={
         "console_scripts": [
